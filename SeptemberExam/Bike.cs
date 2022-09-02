4e8ec4557b64cb8e247a24356a2d4c74d21c2822
mtully0104 <78148305+mtully0104@users.noreply.github.com>
2022-09-02 10:26:54 +0100

    Added vat rate method

---
+++ SeptemberExam/Bike.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeptemberExam
+{
+    public class Bike
+    {
+        //prop
+        public int ID { get; set; }
+        public decimal price { get; set; }
+        public string Description { get; set; }
+
+        //constructors
+
+        //methods
+        public void AddVat(decimal vatRate)
+        {
+            price *= vatRate;
+        }
+
+
+    }
+}
